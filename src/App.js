e5a6b5682d1ec3c9b6d6d7b357e585e08317c4cb
Hao Chen <haoc46@cs.washington.edu>
2018-07-10 17:54:00 -0700

    App basics

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './index.css';
+import 'whatwg-fetch';
+import moment from 'moment';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {page: 0, json: [], date: moment(),}
+    }
+
+
+    componentDidMount() {
+        let currentDate = moment(this.state.date);
+        let arr = [];
+        for (let i = 0; i < 20; i++) {
+
+            arr.push(currentDate.year() + "-" + currentDate.month() + "-" + currentDate.date());
+            currentDate.subtract(1, 'days');
+        }
+        console.log(arr);
+
+        let error = fetch('https://api.nasa.gov/planetary/apod?api_key=EzzKNCDQOcV3fJHd4ab0NQP551lX5ImTaqkZ037e&date=1990-09-09').
+        then((response) => response.json()).then((data)=>{console.log(data)}).catch((err) =>{alert(err.message)});
+
+
+        Promise.all(arr.map((date) => {
+             return fetch('https://api.nasa.gov/planetary/apod?api_key=EzzKNCDQOcV3fJHd4ab0NQP551lX5ImTaqkZ037e&date=' + date)
+                .then((response) => {
+                    return response.json()
+                }).catch((err) => {alert(err.message)})
+        })).then((jsonArr) =>{
+            console.log(jsonArr);
+            this.setState({json:jsonArr});
+        })
+
+    }
+
+    render() {
+        console.log(this.state.page);
+        return (
+            <button onClick={() => {this.setState({page:this.state.page + 1})}}>Hello</button>
+        );
+    }
 }
 
 export default App;
+
+class Paginator extends Component{
+
+}
+
+class Card extends Component{
+    render(){
+        return(<div className="card">
+            <figure>
+                <img src="./road.jpg" alt="cloud"/>
+                <figcaption>Cloud</figcaption>
+            </figure>
+        </div>);
+    }
+}
+
+class CardList extends Component{
+
+}
+
+class PopUp extends Component{
+
+}
